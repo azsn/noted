17ce82d75ed004f6c2fe1779f86a1ac97d4aca75
zelbrium <zelbrium@gmail.com>
2017-11-23 10:41:13 -0800

    Centering canvas in the view area

--- src/cocoa/NCView.swift
+++ src/cocoa/NCView.swift
@@ -210,3 +210,26 @@ class NCView: NSView
 //    }
 //    }
 }
+
+// https://stackoverflow.com/a/28154936/161429
+class CenteredClipView: NSClipView
+{
+    override func constrainBoundsRect(_ proposedBounds: NSRect) -> NSRect
+    {
+        var rect = super.constrainBoundsRect(proposedBounds)
+        if let containerView = self.documentView
+        {
+            if (rect.size.width > containerView.frame.size.width)
+            {
+                rect.origin.x = (containerView.frame.width - rect.width) / 2
+            }
+            
+            if(rect.size.height > containerView.frame.size.height)
+            {
+                rect.origin.y = (containerView.frame.height - rect.height) / 2
+            }
+        }
+        
+        return rect
+    }
+}
