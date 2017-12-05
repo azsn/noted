98a1eba103480ce83d254d4b88f06ddc11218575
zelbrium <zelbrium@gmail.com>
2017-12-04 23:13:28 -0500

    Fixed periodic crashes due to a recreated CGContext

--- src/cocoa/NCView.swift
+++ src/cocoa/NCView.swift
@@ -12,18 +12,11 @@ class NCView: NSView
 {
     static var kPageWidth: CGFloat = 500
     var canvas: OpaquePointer?
-    var surface: OpaquePointer?
-    var cr: OpaquePointer?
-    var surfaceSize: NSSize = NSSize()
     var currentTool: NCInputTool = kNCPenTool
     
     required init?(coder: NSCoder)
     {
         super.init(coder: coder)
-        
-//        let c = noted_canvas_new()
-//        
-//        setCanvas(canvas: c!)
     }
     
     override var acceptsFirstResponder: Bool
@@ -49,77 +42,54 @@ class NCView: NSView
         style.thickness = Float(0.8 / NCView.kPageWidth)
         noted_canvas_set_stroke_style(canvas, style)
     }
-    
-    func ensureSurface() -> Bool
-    {
-        if(self.canvas == nil)
-        {
-            return false
-        }
-        
-        var size = self.frame.size
-        size.width = ceil(size.width)
-        size.height = ceil(size.height)
-        
-        if(self.surface != nil && self.cr != nil && __CGSizeEqualToSize(self.surfaceSize, size))
-        {
-            return true
-        }
-        
-        self.surfaceSize = size
-        
-        if(self.cr != nil)
-        {
-            cairo_destroy(self.cr)
-            self.cr = nil
-        }
-        
-        if(self.surface != nil)
-        {
-            cairo_surface_destroy(self.surface)
-            self.surface = nil
-        }
-        
-        let context = NSGraphicsContext.current()?.cgContext
-        if(context == nil)
-        {
-            return false
-        }
-        
-        self.surface = cairo_quartz_surface_create_for_cg_context(context, UInt32(size.width), UInt32(size.height))
-        if(self.surface == nil || cairo_surface_status(self.surface) != CAIRO_STATUS_SUCCESS)
-        {
-            self.surface = nil
-            return false
-        }
-        
-        self.cr = cairo_create(self.surface)
-        if(self.cr == nil || cairo_status(self.cr) != CAIRO_STATUS_SUCCESS)
-        {
-            self.cr = nil
-            return false
-        }
-        
-        Swift.print("recreated surface")
-        return true;
-    }
-    
+
     override func draw(_ rect: NSRect)
     {
-        if(!ensureSurface())
+        let context = NSGraphicsContext.current()?.cgContext
+        
+        if(self.canvas == nil || context == nil)
         {
             return
         }
         
+        // Create suface
+        // TODO: See if there's a way to not have to create a new
+        // surface/context each draw. Doing so works, until the current
+        // CGContext changes, which happens unpredictably, at which
+        // point the surface breaks.
+        let surface = cairo_quartz_surface_create_for_cg_context(context,
+                                                                 UInt32(self.frame.size.width),
+                                                                 UInt32(self.frame.size.height))
+        if(cairo_surface_status(surface) != CAIRO_STATUS_SUCCESS)
+        {
+            return
+        }
+        
+        // Create cairo context
+        let cr = cairo_create(surface)
+        if(cairo_status(cr) != CAIRO_STATUS_SUCCESS)
+        {
+            return
+        }
+        
+        // Clip
+        cairo_rectangle(cr, Double(rect.origin.x), Double(rect.origin.y), Double(rect.size.width), Double(rect.size.height))
+        cairo_clip(cr)
+        
+        // Scale from canvas coordinates (width 0..1) to screen coordinates
+        cairo_scale(cr, Double(self.frame.size.width), Double(self.frame.size.width))
+
         // Magnification controlled by the NSScrollView ancestor
+        // Used by the canvas to know if it shouldn't bother drawing
+        // beziers because they'd be too small to see on screen.
         let magnification = (self.superview?.superview as? NSScrollView)?.magnification ?? 1
         
-        cairo_save(self.cr)
-        cairo_rectangle(self.cr, Double(rect.origin.x), Double(rect.origin.y), Double(rect.size.width), Double(rect.size.height))
-        cairo_clip(self.cr)
-        cairo_scale(self.cr, Double(self.frame.size.width), Double(self.frame.size.width))
-        noted_canvas_draw(self.canvas, self.cr, Float(magnification))
-        cairo_restore(self.cr)
+        // Draw
+        noted_canvas_draw(canvas, cr, Float(magnification))
+        
+        // Done
+        cairo_destroy(cr)
+        cairo_surface_destroy(surface)
     }
     
     let invalidate_callback : @convention(c) (OpaquePointer?, UnsafeMutablePointer<NCRect>?, UnsafeMutableRawPointer?) -> Void =
@@ -131,7 +101,6 @@ class NCView: NSView
         if(rect?.pointee == nil)
         {
             // Update properties
-            
             let height = noted_canvas_get_height(self.canvas)
             self.setFrameSize(NSMakeSize(NCView.kPageWidth, CGFloat(height) * NCView.kPageWidth))
             self.setNeedsDisplay(self.frame)
@@ -236,3 +205,103 @@ class CenteredClipView: NSClipView
         return rect
     }
 }
+
+//    var surface: OpaquePointer?
+//    var cr: OpaquePointer?
+//    var surfaceSize: NSSize = NSSize()
+
+//    func ensureSurface() -> Bool
+//    {
+//        // Don't bother if there isn't a canvas
+//        if(self.canvas == nil)
+//        {
+//            return false
+//        }
+//
+//        // Can't create a surface without a context
+//        let context = NSGraphicsContext.current()?.cgContext
+//        if(context == nil)
+//        {
+//            return false
+//        }
+//
+//        var contextChanged = false
+//
+//        // If we already have a good surface,
+//        // don't recreate it
+//        if(self.surface != nil
+//            && self.cr != nil
+//            && __CGSizeEqualToSize(self.surfaceSize, self.frame.size))
+//        {
+//            // Check to see if the context that cairo has
+//            // is the same as the current context. If not,
+//            // the surface must be recreated.
+//            if let cctx = cairo_quartz_surface_get_cg_context(self.surface)
+//            {
+//                if(cctx.toOpaque() == Unmanaged<CGContext>.passUnretained(context!).toOpaque())
+//                {
+//                    return true
+//                }
+//            }
+//            contextChanged = true
+//        }
+//
+//        // (Re)create surface
+//
+//        self.surfaceSize = self.frame.size
+//
+//        // Destroy any existing cairo things
+//        if(self.cr != nil)
+//        {
+//            cairo_destroy(self.cr)
+//            self.cr = nil
+//        }
+//        if(self.surface != nil)
+//        {
+////            if(contextChanged)
+////            {
+//                // When the context changes, it seems the old context's stack is
+//                // emptied. This causes cairo to segfault when destroying the
+//                // surface, since it tries to do a stack restore before releasing.
+//                // So push a fake save to the stack.
+//                if let cctx = cairo_quartz_surface_get_cg_context(self.surface)
+//                {
+//                    cctx.takeRetainedValue().saveGState()
+//                }
+////            }
+//            cairo_surface_destroy(self.surface)
+//            self.surface = nil
+//        }
+//
+//        // Create
+//
+//        self.surface = cairo_quartz_surface_create_for_cg_context(context,
+//                                                                  UInt32(self.frame.size.width),
+//                                                                  UInt32(self.frame.size.height))
+//        if(self.surface == nil)
+//        {
+//            self.surface = nil
+//            return false
+//        }
+//        if(cairo_surface_status(self.surface) != CAIRO_STATUS_SUCCESS)
+//        {
+//            cairo_surface_destroy(self.surface)
+//            self.surface = nil
+//            return false
+//        }
+//
+//        self.cr = cairo_create(self.surface)
+//        if(self.cr == nil)
+//        {
+//            self.cr = nil
+//            return false
+//        }
+//        if(cairo_status(self.cr) != CAIRO_STATUS_SUCCESS)
+//        {
+//            cairo_destroy(self.cr)
+//            self.cr = nil
+//            return false
+//        }
+//        return true;
+//    }
+
